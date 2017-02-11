53fb9e0b3707944b1940f7c78439a2f4b13dfe30
Romsn90 <Roman.Pass@tum.de>
2017-02-11 13:40:46 +0100

    Update authService.js

--- src/components/Services/authService.js
+++ src/components/Services/authService.js
@@ -1,8 +1,8 @@
 angular.module("sc-auth", [])
 .service('authService', function(scAuth) {
 
-    var USER_LOGIN = "roman.pass@tum.de";
-    var USER_PASSWORD = "8r19ada";
+    var USER_LOGIN = "";
+    var USER_PASSWORD = "";
 
     var authentication = function(scCallback) {
         scAuth.login(USER_LOGIN, USER_PASSWORD, scCallback, scError);
